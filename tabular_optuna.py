720fc1a8022931effc460a83e0084e2c5596d0a3
Slawek Biel <slawek.biel@gmail.com>
2021-09-16 14:51:32 +0200

    CV .218 LB 2088

--- tabular_optuna.py
+++ tabular_optuna.py
@@ -11,36 +11,57 @@ def rmspe(preds, targs):
     return (x**2).mean().sqrt()
 
 
+def get_dls(df_train, splits):
+    cont_nn,cat_nn = cont_cat_split(df_train, max_card=9000, dep_var='target')
+    cat_nn.remove('time_id'), cat_nn.remove('row_id')
+    #procs_nn = [Categorify,FillMissing, Normalize]
+    procs_nn = [Categorify, Normalize]
+    to_nn = TabularPandas(df_train.fillna(0), procs_nn, cat_nn, cont_nn,
+                      splits=splits, 
+                       
+                      y_names='target')
+
+    return to_nn.dataloaders(1024)
+
 def train_network(trial, dls):
-    n_layers = trial.suggest_int('n_layers',2,5)
+    n_layers = trial.suggest_int('n_layers',2,6)
     dropouts = []
     layer_sizes = []
     for i in range(n_layers):
-        dropouts.append(trial.suggest_float(f'p{i}', 0, .5))
-        layer_sizes.append(trial.suggest_int(f'layer_{i}', 100, 1000))
+        p = 0 if i==0 else trial.suggest_float(f'p{i}', 0, .5)
+        dropouts.append(p)
+        layer_sizes.append(trial.suggest_int(f'layer_{i}', 10, 1000))
     embed_p = trial.suggest_float('embed_p', 0, .5)
-    config = {'ps': dropouts, 'embed_p':embed_p}
-    lr = trial.suggest_float('lr', 1e-3, 2e-2, log=True)
+    config = {'ps': dropouts, 'embed_p':embed_p, 'lin_first':False}
+    lr = trial.suggest_float('lr', 1e-3, 2e-2)
     learn = tabular_learner(dls, y_range=(0,.1), layers=layer_sizes, 
-                        n_out=1, loss_func = rmspe, metrics=AccumMetric(rmspe), config=config, cbs=FastAIPruningCallback(trial, monitor="rmspe"))
-    #with learn.no_bar():
-    with learn.no_logging():
-        learn.fit_one_cycle(50, lr)
-    return learn.recorder.metrics[0].value
+                        n_out=1, loss_func = rmspe, metrics=AccumMetric(rmspe), config=config)
+    with learn.no_bar():
+        with learn.no_logging():
+            learn.fit_one_cycle(50, lr)
+    last5 = L(learn.recorder.values).itemgot(2)[-5:]
+    return np.mean(last5)
+
+
+def train_cross_valid(trial, dlss):
+    res = 0
+    for idx, dls in enumerate(dlss):
+        v = train_network(trial, dls)
+        print(f'fold {idx}: {v}')
+        res += v/5
+    print('cross valid:', res)
+    return res
 
 if __name__ == '__main__':
-    df_train = pd.read_csv('train_with_features_NO_ST.csv')
-    trn_idx, val_idx = first(GroupKFold().split(df_train, groups = df_train.time_id))
-    splits=[list(trn_idx), list(val_idx)]
-    cont_nn,cat_nn = cont_cat_split(df_train, max_card=9000, dep_var='target')
-    cat_nn.remove('time_id')
-    cat_nn.remove('row_id')
-    procs_nn = [Categorify,FillMissing, Normalize]
-    dls = TabularPandas(df_train, procs_nn, cat_nn, cont_nn,
-                      splits=splits, y_names='target').dataloaders(1024)
+    df_train = pd.read_csv('train_with_features.csv')
+    df_train = df_train.fillna(0)
+    print('loading datasets...')
+    dlss = [get_dls(df_train, [list(trn_idx), list(val_idx)]) for trn_idx, val_idx in GroupKFold().split(df_train, groups = df_train.time_id)]
+    print('creating study..')
 
-    pruner = optuna.pruners.MedianPruner(n_warmup_steps=3)
-    study = optuna.create_study(direction="minimize", pruner=pruner, study_name = 'tabular_learner_50_epochs', storage='sqlite:///optuna.db',load_if_exists=True)
 
-    study.optimize(functools.partial(train_network, dls=dls))
+
+    study = optuna.create_study(direction="minimize", study_name = 'tabular_learner_features', storage='sqlite:///optuna.db',load_if_exists=True)
+
+    study.optimize(functools.partial(train_cross_valid, dlss=dlss))
 
